9fafd16f34220b727e399061fa6dd2cd35742f44
артур <gumarovartur059@example.com>
2025-07-08 16:36:35 +0300

    08.07.2025 good job

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { FilterPanel } from 'components/FilterPanel';
-import { JobList } from 'components/JobList';
-import { TheHeader } from 'components/TheHeader';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { FilterPanel } from "components/FilterPanel";
+import { JobList } from "components/JobList";
+import { TheHeader } from "components/TheHeader";
+
+import { addPositions } from "./store/positions/positions-actions";
+
+import data from "./mock/data.json";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addPositions(data));
+  });
+
   return (
     <>
-     <TheHeader />
-     <div className='container'>
-      <FilterPanel />
-      <JobList />
-     </div>
+      <TheHeader />
+      <div className="container">
+        <FilterPanel />
+        <JobList />
+      </div>
     </>
   );
 }
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,17 @@
-import data from '../mock/data.json';
-import { JobPosition } from './JobPosition';
+import { useSelector } from "react-redux";
+import { JobPosition } from "./JobPosition";
+import { selectAllPositions } from "store/positions/positions-selectors";
 
 const JobList = () => {
+  const positions = useSelector(selectAllPositions);
+
   return (
-    <div className='job-list'>
-      {data.map(item => (
+    <div className="job-list">
+      {positions.map((item) => (
         <JobPosition key={item.id} {...item} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export {JobList};
+export { JobList };
